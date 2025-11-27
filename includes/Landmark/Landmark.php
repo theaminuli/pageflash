98945a62bf65aec8ae10abce1927c212917857ef
theaminuldev <mdaminulislam2015bd@gmail.com>
2025-11-27 21:42:02 +0600

    Remove unused General import and instantiation
    Commented out the import and instantiation of the General class in Landmark.php, indicating it is not currently used. Also simplified the pageflash_init_landmark method to only instantiate NoReload.

--- includes/Landmark/Landmark.php
+++ includes/Landmark/Landmark.php
@@ -5,7 +5,7 @@ namespace PageFlash\Landmark;
 use PageFlash\Landmark\LandmarkList;
 use PageFlash\Landmark\LandmarkAPI;
 use PageFlash\Landmark\NoReload\NoReload;             
-use PageFlash\Landmark\General\General;
+// use PageFlash\Landmark\General\General;
 
 class Landmark {
 
@@ -23,30 +23,8 @@ class Landmark {
 	 * Initialize Landmark features.
 	 */
 	public function pageflash_init_landmark() {
-<<<<<<< Updated upstream
-		$landmarks     = get_option( 'pageflash_landmarks', array() );
-		$landmark_data = $landmarks['data'] ?? array();
-
-		foreach ( $landmark_data as $slug => $item ) {
-			if ( ! empty( $item['active'] ) ) {
-				switch ( $slug ) {
-					case 'quicklink':
-						new NoReload\Quicklink();
-						break;
-					case 'noreload':
-						new NoReload\NoReload();
-						break;
-					case 'instantpage':
-						// TODO: Implement InstantPage functionality.
-						break;
-					// Add more cases here as you add more landmark features.
-				}
-			}
-		}
-=======
 		new NoReload();
-		new General();
->>>>>>> Stashed changes
+		// new General();
 	}
 
 	/**
