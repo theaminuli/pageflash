22c712c81a3e8a3d46ce7ff692362997a6b989f8
theaminuldev <mdaminulislam2015bd@gmail.com>
2026-01-24 16:59:21 +0600

    Add IcoMoon font files and heartbeat stub
    Added IcoMoon font files (eot, svg, ttf, woff) to assets for icon support. Introduced a disabled heartbeat.js stub to prevent heartbeat functionality.

---
+++ assets/libs/heartbeat/heartbeat.js
@@ -0,0 +1,8 @@
+/**
+ * Disabled Heartbeat Stub
+ * This is an empty stub to prevent heartbeat functionality
+ */
+(function($) {
+    'use strict';
+    // Intentionally empty - heartbeat is disabled
+})(jQuery);
