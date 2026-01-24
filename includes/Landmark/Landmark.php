cc0403bfd845bdefebc08500eb0bcc57dc0df4ab
theaminuldev <mdaminulislam2015bd@gmail.com>
2026-01-24 17:04:33 +0600

    Refactor Landmark class and extend LandmarkList options
    Refactored the Landmark class to introduce a feature registration and instance management system, improving extensibility and maintainability. Updated LandmarkList to add new general options for REST API, XML-RPC, WordPress version hiding, and jQuery Migrate, enhancing security and performance configuration capabilities.

--- includes/Landmark/Landmark.php
+++ includes/Landmark/Landmark.php
@@ -2,36 +2,101 @@
 
 namespace TheAminul\PageFlash\Landmark;
 
-use TheAminul\PageFlash\Landmark\LandmarkList;
-use TheAminul\PageFlash\Landmark\LandmarkAPI;
 use TheAminul\PageFlash\Landmark\NoReload\NoReload;
-// use TheAminul\PageFlash\Landmark\General\General;
+use TheAminul\PageFlash\Landmark\General\General;
 
 class Landmark {
 
 	/**
-	 * Initialize the Landmark.
+	 * Instance registry.
 	 *
-	 * @since PageFlash 1.0.0
+	 * @var array<string, object>
+	 */
+	protected array $instances = array();
+
+	/**
+	 * Landmark constructor.
+	 *
+	 * @since PageFlash 1.3.0
 	 */
 	public function __construct() {
-		$this->pageflash_register_landmarks();
-		$this->pageflash_init_landmark();
+		add_action( 'pageflash_init', array( $this, 'boot' ) );
+		add_action( 'pageflash_init', array( $this, 'register_landmarks' ) );
+		// $this->register_landmarks();
 	}
 
 	/**
-	 * Initialize Landmark features.
+	 * Plugin bootstrap.
+	 *
+	 * @since 1.3.0
 	 */
-	public function pageflash_init_landmark() {
-		new NoReload();
-			// new General();
+	public function boot(): void {
+		$this->register_features();
+		$this->init_managers();
 	}
 
-		/**
-		 * Register LandmarkList and LandmarkAPI.
-		 */
-	public function pageflash_register_landmarks() {
-		new LandmarkList();
-		new LandmarkAPI();
+	/**
+	 * Feature class list.
+	 *
+	 * @since 1.3.0
+	 * @return array<class-string>
+	 */
+	protected function features(): array {
+		return array(
+			NoReload::class,
+			General::class,
+		);
+	}
+
+	/**
+	 * Register all features.
+	 *
+	 * @since 1.3.0
+	 */
+	protected function register_features(): void {
+		foreach ( $this->features() as $feature ) {
+			$feature::init_register();
+		}
+
+		do_action( 'pageflash_register_features' );
+	}
+
+	/**
+	 * Initialize feature managers.
+	 *
+	 * @since 1.3.0
+	 */
+	protected function init_managers(): void {
+		foreach ( $this->features() as $feature ) {
+			$this->get_instance( $feature );
+		}
+
+		do_action( 'pageflash_init_managers', $this );
+	}
+
+	/**
+	 * Register landmark services.
+	 *
+	 * @since PageFlash 1.0.0
+	 */
+	public function register_landmarks(): void {
+
+		$this->get_instance( LandmarkList::class );
+		$this->get_instance( LandmarkAPI::class );
+	}
+
+	/**
+	 * Get or create an instance.
+	 *
+	 * @since 1.3.0
+	 * @param class-string $class
+	 * @return object
+	 */
+	public function get_instance( string $class ): object {
+		if ( isset( $this->instances[ $class ] ) ) {
+			return $this->instances[ $class ];
+		}
+
+		return $this->instances[ $class ] = new $class();
 	}
 }
